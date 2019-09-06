02920b72a9aff33d7efb90ef39b9a546e248c6e5
sagar.saindre <sagar.saindre@wgblindia.com>
2019-09-06 20:06:16 +0530

    Responsive css added

--- js/custom.js
+++ js/custom.js
@@ -17,32 +17,48 @@ $(document).ready(function(){
 
     curousalImage: function() {
       $(document).ready(function(){
-         $('#owl-carousel').owlCarousel({
-            loop: true,
-            margin: 50,
-            items: 2,
-            responsiveClass: true,
-            responsive: {
-                0: {
-                    items: 2,
-                    nav: true
-                },
-                600: {
-                    items: 2,
-                    nav: false
-                },
-                768: {
-                    items: 3,
-                    nav: false
-                },
-                1000: {
-                    items: 3,
-                    nav: true,
-                    loop: true
-                }
+        $('#owl-carousel').owlCarousel({
+          loop: true,
+          margin: 50,
+          items: 2,
+          nav: true,
+          navClass: ['owl-prev', 'owl-next'],
+          responsiveClass: true,
+          onChange: function (elem) {
+      var current = this.currentItem;
+      var src = elem.find(".owl-item").eq(current).find("img").attr('src');
+      console.log('Image current is ' + src);
+    },
+          responsive: {
+            0: {
+              items: 2,
+              nav: true
+            },
+            600: {
+              items: 2,
+              nav: false
+            },
+            768: {
+              items: 3,
+              nav: false
+            },
+            1000: {
+              items: 3,
+              nav: true,
+              loop: true
             }
+          }
         });
-    });
+
+        // var vartcarousel = $("#owl-carousel");
+        // // vartcarousel.owlCarousel(owlcarousel_obj);
+        // $(".owl-prev").on("click touch", function(){
+        //     vartcarousel.trigger("next.owl.carousel");
+        // });
+        // $(".owl-next").on("click touch", function(){
+        //     vartcarousel.trigger("prev.owl.carousel");
+        // });
+      });
     },
 
     curousalTestimonials: function() {
